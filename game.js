afce166c9764efeff2801f6437f9d8ed7f9b2752
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-11 10:55:33 -0300

    This is the change I made to the game.js and index.html
    Added three buttons to index.html representing each player's choice and a reset button.
    Created two variables to store scores for each player according to the game results.
    Added div elements for displaying the game results and the score.
    Implemented a scoring system in the playRound function to keep track of points for each player.
    Announced the game winner after one player reaches 5 points.

--- game.js
+++ game.js
@@ -3,42 +3,101 @@ function getComputerChoice() {
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", function() {
+    playerSelectionScore = 0;
+    computerSelectionScore = 0;
+
+    result.textContent = "";
+    score.textContent = "";
+
+});
+
+rockButton.addEventListener("click", function() {
+    const playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+paperButton.addEventListener("click", function() {
+    const playerSelection = "Paper";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    });
+
+scissorsButton.addEventListener("click", function() {
+    const playerSelection = "Scissors";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+let playerSelectionScore = 0;
+let computerSelectionScore = 0;
+
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Paper') {
-        return window.alert('You lost! Paper beats rock!');
+    if (playerSelectionScore >= 5) {
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
+        result.textContent = "Congratulations! You won the game!";
+        return;
+    }
+    else if (computerSelection >= 5) {
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
+        result.textContent = "You failed! Computer won the game!";
+        return;
+    }
+    else if (playerSelection.toLowerCase() == 'Rock' && computerSelection == 'Paper') {
+        result.textContent = 'You lost! Paper beats rock!';
+        computerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Rock') {
-        return window.alert('A tie!');
+        result.textContent = 'A tie!';
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors') {
-        return window.alert('You won! Rock beats scissors!');
+        result.textContent = 'You won! Rock beats scissors!';
+        playerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection ==  'Paper') {
-        return window.alert('A tie!');
+        result.textContent = 'A tie!';
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
-        return window.alert('You won! Paper beats rock!');
+        result.textContent = 'You won! Paper beats rock!';
+        playerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors') {
-        return window.alert('You lost! Scissors beats paper!');
+        result.textContent = 'You lost! Scissors beats paper!';
+        computerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
-        return window.alert('You won! Scissors beats paper!');
+        result.textContent = 'You won! Scissors beats paper!';
+        playerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Rock') {
-        return window.alert('You lost! Scissors beats rock!');
+        result.textContent = 'You lost! Scissors beats rock!';
+        computerSelectionScore++
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
     else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Scissors') {
-        return window.alert('A tie!');
+        result.textContent = 'A tie!';
+        score.textContent = playerSelectionScore + " points for player  VS " + computerSelectionScore + " points for computer ";
     }
 }
-function game() {
-    const playerSelection = window.prompt("Rock, Paper or Scissors?");
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
-    console.log(result)
-}
-
-game();
+const container = document.querySelector('#container');
+const result = document.createElement('div');
+const score = document.createElement('div');
+score.classList.add('score');
+result.classList.add('result');
+container.appendChild(result);
+container.appendChild(score);
 
